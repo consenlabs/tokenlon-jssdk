e6c9e8702c718bf28b3d552b6274bd28dfbd9e32
Xaber <xaber@foxmail.com>
2020-02-28 11:13:00 +0800

    chore: add invalid config params check

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,5 @@
+import * as _ from 'lodash'
+import { JSSDK_ERRORS } from '../utils/errors'
 import { IConfig, SignRawTransactionFnParams } from '../global'
 
 const config = {
@@ -8,6 +10,9 @@ const config = {
 }
 
 export const setConfig = (params: IConfig): IConfig => {
+  if (!_.isObject(params)) throw JSSDK_ERRORS.INVALID_CONFIG_PARAMS
+  const { personalSignFn, signRawTransactionFn } = params
+  if (!_.isFunction(personalSignFn) || !_.isFunction(signRawTransactionFn)) throw JSSDK_ERRORS.INVALID_CONFIG_PARAMS
   return Object.assign(config, params)
 }
 
